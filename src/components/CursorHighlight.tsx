e6304da4661189110a0bb88bb27f4b6e6235ca54
ls3205 <ls3205@trevor.org>
2024-04-02 13:20:35 -0400

    cursor highlight update

--- src/components/CursorHighlight.tsx
+++ src/components/CursorHighlight.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useSpring } from "framer-motion";
 
 interface CursorHighlightProps {}
 
 const CursorHighlight: React.FC<CursorHighlightProps> = ({}) => {
     const cursorX = useMotionValue(-100);
     const cursorY = useMotionValue(-100);
+
+    const springConfig = { damping: 60, stiffness: 1000 };
+    const cursorXSpring = useSpring(cursorX, springConfig);
+    const cursorYSpring = useSpring(cursorY, springConfig);
 
     useEffect(() => {
         const moveCursor = (e: MouseEvent) => {
@@ -21,10 +25,10 @@ const CursorHighlight: React.FC<CursorHighlightProps> = ({}) => {
 
     return (
         <motion.div
-            className="invisible fixed left-4 top-4 h-0 w-0 rounded-full bg-primary shadow-[0px_0px_40px_20px] shadow-primary md:visible"
+            className="invisible fixed left-4 top-4 h-0 w-0 rounded-full bg-primary shadow-[0px_0px_80px_25px] shadow-primary md:visible"
             style={{
-                translateX: cursorX,
-                translateY: cursorY,
+                translateX: cursorXSpring,
+                translateY: cursorYSpring,
             }}
         />
     );
